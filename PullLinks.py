bd01a88b2e4d2bd413c80844d7a47acba3b4a9f5
KhushPar04 <159176248+KhushPar04@users.noreply.github.com>
2024-03-24 02:59:27 +0000

    url + link pull

--- PullLinks.py
+++ PullLinks.py
@@ -19,25 +19,3 @@ def get_sublinks(url):
 url = "https://bloustein.rutgers.edu/"  # Replace with the website you are interested in
 sublinks = get_sublinks(url)
 print(sublinks)
-
-# URLs to scrape
-#urls = [
-###"https://newbrunswick.rutgers.edu/academics/schools-colleges/school-of-arts-and-sciences",
-#"https://bloustein.rutgers.edu/",
-###"https://gsapp.rutgers.edu/",
-#"https://gse.rutgers.edu/",
-#"https://www.masongross.rutgers.edu/",
-#"https://www.business.rutgers.edu/","https://sas.rutgers.edu/",
-#"https://comminfo.rutgers.edu/",
-#"https://soe.rutgers.edu/apply",
-#"https://sebs.rutgers.edu/",
-#"https://grad.rutgers.edu/",
-###"https://smlr.rutgers.edu/",
-#"https://socialwork.rutgers.edu/",
-#"https://pharmacy.rutgers.edu/",
-#"https://njms.rutgers.edu/",
-#"https://rwjms.rutgers.edu/",
-#"https://sdm.rutgers.edu/",
-#"https://shp.rutgers.edu/",
-#"https://nursing.rutgers.edu/",
-#"https://sph.rutgers.edu/" ]
